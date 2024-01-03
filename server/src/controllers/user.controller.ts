0862d73ef62f63599841d2c73d6859bca4002e64
Dhanasai Tholeti <dhanasai5566@gmail.com>
2024-01-03 08:20:14 +0530

    used the error middleware instead of direclty sending error response in user controllers

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import getUserDataWithEmail from "../libs/helpers/getDataWithEmail";
 import { tokenGenerator } from "../libs/helpers/tokenGenerator";
 import userModel from "../models/user.model";
+import { ErrorWithStatusCode } from "../libs/types/error.types";
 
 export const userSignin = async (
   req: Request,
@@ -10,23 +11,27 @@ export const userSignin = async (
   next: NextFunction
 ) => {
   const { email, password } = req.body;
-  console.log(req.body);
 
   try {
     const EmailUsersData = await getUserDataWithEmail(email);
-    if (!EmailUsersData)
-      return res.status(403).json({ message: "Invalid Credentials" });
+    if (!EmailUsersData) {
+      const error: ErrorWithStatusCode = new Error("Invalid Credentials");
+      error.statusCode = 403;
+      throw error;
+    }
 
     const passwdRes = await bcrypt.compare(
       password,
       EmailUsersData.password_hash
     );
 
-    if (!passwdRes)
-      return res
-        .status(403)
-        .json({ message: "Password provided is incorrect" });
-
+    if (!passwdRes) {
+      const error: ErrorWithStatusCode = new Error(
+        "Password provided is incorrect"
+      );
+      error.statusCode = 403;
+      throw error;
+    }
     const token = await tokenGenerator({
       UserId: EmailUsersData.id,
       Email: EmailUsersData.email,
@@ -53,10 +58,14 @@ export const userSignup = async (
   const { email, name, password } = req.body;
   try {
     const EmailUsersData = await getUserDataWithEmail(email);
-    if (EmailUsersData)
-      return res.send(400).json({
-        message: "Email is already in use, Try with a different email",
-      });
+
+    if (EmailUsersData) {
+      const error: ErrorWithStatusCode = new Error(
+        "Email is already in use, Try with a different email"
+      );
+      error.statusCode = 400;
+      throw error;
+    }
 
     const salt = await bcrypt.genSalt(10);
     const password_hash = await bcrypt.hash(password, salt);
--- server/src/middlewares/errorHandler.middleware.ts
+++ server/src/middlewares/errorHandler.middleware.ts
@@ -8,8 +8,8 @@ const errorHandler = (
   _next: NextFunction
 ) => {
   const statusCode = err.statusCode || 500;
+
   res.status(statusCode).json({ error: err.message });
-  res.status(500).json({ error: "something went wrong lol:)" });
 };
 
 export default errorHandler;
